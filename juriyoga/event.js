29681a2b6cf48cfd774e5213b1cb0fca3dc25b57
aokingAK <120313898+aokingAK@users.noreply.github.com>
2024-12-27 19:33:24 +0900

    Add files via upload

---
+++ juriyoga/event.js
@@ -0,0 +1,19 @@
+$('.photo').slick({
+    autoplay: true,         //自動再生
+    autoplaySpeed: 1,       //自動再生のスピード
+    speed: 10000,            //スライドスピード
+    slidesToShow: 4,        //スライドの表示枚数
+    cssEase: "linear",      //スライドの動きを等速に
+    pauseOnHover: false,    //ホバーしても止まらないように
+    pauseOnFocus: false,    //フォーカスしても止まらないように
+});
+
+// $('.photo').slick({
+//     autoplay: true,         //自動再生
+//     autoplaySpeed: 1,       //自動再生のスピード
+//     speed: 10000,            //スライドスピード
+//     slidesToShow: 4,        //スライドの表示枚数
+//     cssEase: "linear",      //スライドの動きを等速に
+//     pauseOnHover: false,    //ホバーしても止まらないように
+//     pauseOnFocus: false,    //フォーカスしても止まらないように
+// });
