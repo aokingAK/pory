5da360d49ffc095ee3841944986b3c7e0b16c058
aokingAK <120313898+aokingAK@users.noreply.github.com>
2025-01-15 11:40:50 +0900

    Add files via upload

--- juriyoga/event.js
+++ juriyoga/event.js
@@ -26,3 +26,32 @@ $(function () {
         rtl: true
     });
 });
+
+$(function () {
+    // ウィンドウをスクロールしたら…
+    $(window).scroll(function () {
+        // ウィンドウの高さを取得
+        const wHeight = $(window).height();
+        // スクロールした量を取得
+        const wScroll = $(window).scrollTop();
+            // それぞれのblockクラスに対して…
+            $(".fadeInUp,.fadeInDown,.fadeInLeft,.fadeInRight").each(function () {
+                // それぞれのblockクラスのウィンドウからの高さを取得
+                const bPosition = $(this).offset().top;
+                // スクロールした量が要素の高さを上回ったら
+                // その数値にウィンドウの高さを引き、最後に200pxを足す
+            if (wScroll > bPosition - wHeight + 200) {
+                $(this).addClass("fadeIn");
+            }
+        });
+    });
+});
+
+// クラスの付け外しのみ
+const text = document.querySelector('.text');
+
+text.classList.add('is-active');
+
+setInterval(() => {
+  text.classList.toggle('is-active');
+}, 3000);
